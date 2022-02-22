514a7c3a67a7798cc1ad71d89fe04a0371a1cb26
Jeffry Gonzalez <jeff@hypertheory.com>
2022-02-22 16:35:19 -0500

    have the bonus calculator ready

---
+++ BankingDomain/StandardBonusCalculator.cs
@@ -0,0 +1,18 @@
+﻿namespace BankingDomain
+{
+    public class StandardBonusCalculator
+    {
+       
+
+        public decimal GetBonusFor(decimal balance, decimal amountOfDeposit)
+        {
+           if(balance >= 10000) // JFHCI
+            {
+                return amountOfDeposit * .10M;
+            }  else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ BankingUnitTests/StandardBonusCalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿
+using BankingDomain;
+
+namespace BankingUnitTests;
+
+public class StandardBonusCalculatorTests
+{
+    [Fact]
+    public void AccountsWithAppropriateBalanceGetDeposit()
+    {
+        var bonusCalculator = new StandardBonusCalculator();
+        var balance = 10000M;
+        var amountOfDeposit = 100M;
+
+        decimal  bonus = bonusCalculator.GetBonusFor(balance, amountOfDeposit);
+
+        Assert.Equal(10, bonus);
+   
+    }
+
+    [Fact]
+    public void AccountsBelowCutoffGetNoBalance()
+    {
+        var bonusCalculator = new StandardBonusCalculator();
+        var balance = 9999.99M;
+        var amountOfDeposit = 100M;
+
+        decimal bonus = bonusCalculator.GetBonusFor(balance, amountOfDeposit);
+
+        Assert.Equal(0, bonus);
+    }
+}
+    
