2a1ddd515ae1442dfb29fdbbce4719494ee7febd
Jeffry Gonzalez <jeff@hypertheory.com>
2022-02-23 16:16:56 -0500

    clock based business rule for bonuses

--- BankingDomain/StandardBonusCalculator.cs
+++ BankingDomain/StandardBonusCalculator.cs
@@ -7,16 +7,36 @@
         //{
         //   return GetBonusFor(balance, amountToDeposit);
         //}
+        private readonly IProvideTheBusinessClock _businessClock;
+
+        public StandardBonusCalculator(IProvideTheBusinessClock businessClock)
+        {
+            _businessClock = businessClock;
+        }
 
         public decimal GetBonusFor(decimal balance, decimal amountOfDeposit)
         {
-           if(balance >= 4000) // JFHCI
+            var bonusModifier = .10M;
+            if (_businessClock.IsDuringBusinessHours())
             {
-                return amountOfDeposit * .10M;
-            }  else
+                bonusModifier += .01M;
+
+            }
+
+            if (balance >= 4000) // JFHCI
+            {
+                return amountOfDeposit * bonusModifier;
+            }
+            else
             {
                 return 0;
             }
+
+        }
+
+        private bool DuringBusinessHours()
+        {
+            return DateTime.Now.Hour >= 9 && DateTime.Now.Hour < 17;
         }
 
         decimal IDoBonusCalculations.ForDeposit(decimal balance, decimal amountToDeposit)
--- BankingUnitTests/StandardBonusCalculatorTests.cs
+++ BankingUnitTests/StandardBonusCalculatorTests.cs
@@ -6,22 +6,57 @@ namespace BankingUnitTests;
 public class StandardBonusCalculatorTests
 {
     [Fact]
-    public void AccountsWithAppropriateBalanceGetDeposit()
+    public void AccountsWithAppropriateBalanceGetBonusDuringBusinessHours()
     {
-        var bonusCalculator = new StandardBonusCalculator();
+        // Given - it is during business hours and they have a big enough balance
+        var stubbedClock = new Mock<IProvideTheBusinessClock>();
+        var bonusCalculator = new StandardBonusCalculator(stubbedClock.Object);
+        stubbedClock.Setup(c => c.IsDuringBusinessHours()).Returns(true);
         var balance = 4000M;
         var amountOfDeposit = 100M;
 
         decimal  bonus = bonusCalculator.GetBonusFor(balance, amountOfDeposit);
 
-        Assert.Equal(10, bonus);
+        Assert.Equal(11, bonus);
    
     }
 
     [Fact]
-    public void AccountsBelowCutoffGetNoBalance()
+    public void AccountsWithAppropriateBalanceGetBonusOutsideBusinessHours()
     {
-        var bonusCalculator = new StandardBonusCalculator();
+        // Given - it is outside business hours they don't get the additional bonus
+        var stubbedClock = new Mock<IProvideTheBusinessClock>();
+        var bonusCalculator = new StandardBonusCalculator(stubbedClock.Object);
+        stubbedClock.Setup(c => c.IsDuringBusinessHours()).Returns(false);
+        var balance = 4000M;
+        var amountOfDeposit = 100M;
+
+        decimal bonus = bonusCalculator.GetBonusFor(balance, amountOfDeposit);
+
+        Assert.Equal(10, bonus);
+
+    }
+
+    [Fact]
+    public void AccountsBelowCutoffGetNoBalanceDuringBusinessHours()
+    {
+        var stubbedClock = new Mock<IProvideTheBusinessClock>();
+        var bonusCalculator = new StandardBonusCalculator(stubbedClock.Object);
+        stubbedClock.Setup(c => c.IsDuringBusinessHours()).Returns(true);
+        var balance = 3999.99M;
+        var amountOfDeposit = 100M;
+
+        decimal bonus = bonusCalculator.GetBonusFor(balance, amountOfDeposit);
+
+        Assert.Equal(0, bonus);
+    }
+
+    [Fact]
+    public void AccountsBelowCutoffGetNoBalanceOutsideBusinessHours()
+    {
+        var stubbedClock = new Mock<IProvideTheBusinessClock>();
+        var bonusCalculator = new StandardBonusCalculator(stubbedClock.Object);
+        stubbedClock.Setup(c => c.IsDuringBusinessHours()).Returns(false);
         var balance = 3999.99M;
         var amountOfDeposit = 100M;
 
