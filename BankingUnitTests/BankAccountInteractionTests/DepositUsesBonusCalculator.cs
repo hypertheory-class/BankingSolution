f8f72ab29b972413a2e19b933f601f444f0b8f08
Jeffry Gonzalez <jeff@hypertheory.com>
2022-02-23 12:15:02 -0500

    before switch to Moq

---
+++ BankingUnitTests/BankAccountInteractionTests/DepositUsesBonusCalculator.cs
@@ -0,0 +1,19 @@
+﻿
+namespace BankingUnitTests.BankAccountInteractionTests;
+
+public class DepositUsesBonusCalculator
+{
+
+    [Fact]
+    public void BonusCalculatorIsUsed()
+    {
+
+        // so, if the bank account has a balance of 5000, and I deposit 100, then 42 should be added as bonus to the account.
+
+        var account = new BankAccount(new TestDoubles.StubbedBonusCalculator());
+
+        account.Deposit(100);
+
+        Assert.Equal(5000M + 100 + 42, account.GetBalance());
+    }
+}
---
+++ BankingUnitTests/TestDoubles/StubbedBonusCalculator.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace BankingUnitTests.TestDoubles;
+
+public class StubbedBonusCalculator : IDoBonusCalculations
+{
+    public decimal ForDeposit(decimal balance, decimal amountToDeposit)
+    {
+        if(balance == 5000M && amountToDeposit == 100)
+        {
+            return 42;
+        } else
+        {
+            return 0;
+        }
+    }
+}
